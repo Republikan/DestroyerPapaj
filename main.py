c1b3c087f91d73820931889c5214da32136c7b0c
Republikan <58981998+Republikan@users.noreply.github.com>
2022-04-17 21:37:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 
-TOKEN = "OTM2NzE2MzkwNDI4MjA1MDg3.YfRPHg.sz0FHIybbG1OAEK5Uzg8ozHqMcI"      # Put your Bot token here
+TOKEN = "NzQxNzQ4NTY0MzkyNDExMTk2.Xy8FLg.rD-Ttz_PVqBH3ImCqmfFQSacG88"      # Put your Bot token here
 SKIP_BOTS = False
 
 
