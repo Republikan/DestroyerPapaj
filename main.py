c464472dadb4b718d2ddf008d2146e0a8ee557f1
Republikan <58981998+Republikan@users.noreply.github.com>
2022-04-17 21:37:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ SKIP_BOTS = False
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('Logged in!')
-    for member in client.get_all_members():
-        if member.bot and SKIP_BOTS:
-            continue
-        await member.ban(reason="Banned by BanBot", delete_message_days=7)
-        print(f"Banned {member.display_name}!")
-    print("Banning is complete!")
 
-client.run(OTM2NzE2MzkwNDI4MjA1MDg3.YfRPHg.sz0FHIybbG1OAEK5Uzg8ozHqMcI)
+
+client.run(NzQxNzQ4NTY0MzkyNDExMTk2.Xy8FLg.rD-Ttz_PVqBH3ImCqmfFQSacG88)
